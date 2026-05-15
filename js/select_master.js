bae69ac67517d25ead900c7aec42fa6ac428ff0c
aleksandrroz <swimer.minecraft@gmail.com>
2026-05-15 22:30:14 +0300

    ctrl alt l

--- js/select_master.js
+++ js/select_master.js
@@ -1,37 +1,38 @@
+const TICKET_COST = 12;
+let selectedTickets = 1;
 
-	const TICKET_COST = 12;
- 	let selectedTickets = 1;
+function getTotalCost() {
+  return TICKET_COST * selectedTickets;
+}
 
- 	function getTotalCost() {
- 		return TICKET_COST * selectedTickets;
- 	}
+function updateDisplayCost() {
+  document.getElementById('result-cost').innerText = selectedTickets === 67 ? 'сыкс сэвэн' : (getTotalCost() + '')
+}
 
- 	function updateDisplayCost() {
- 		document.getElementById('result-cost').innerText = selectedTickets === 67 ? 'сыкс сэвэн' : (getTotalCost()+'')
- 	}
- 	function updateTicketCount() {
- 		document.getElementById('result-tickets').innerText = selectedTickets === 67 ? 'сыкс сэвэн' : (selectedTickets+'')
- 	}
+function updateTicketCount() {
+  document.getElementById('result-tickets').innerText = selectedTickets === 67 ? 'сыкс сэвэн' : (selectedTickets + '')
+}
 
- 	function updateDisplay() {
- 		updateDisplayCost();
- 		updateTicketCount();
+function updateDisplay() {
+  updateDisplayCost();
+  updateTicketCount();
 
- 		document.getElementById('calculator-minus').disabled = selectedTickets <= 1;
- 	}
+  document.getElementById('calculator-minus').disabled = selectedTickets <= 1;
+}
 
- 	updateDisplay();
+updateDisplay();
 
 function plusTicket() {
-	selectedTickets++;
-	updateDisplay();
+  selectedTickets++;
+  updateDisplay();
 }
+
 function minusTicket() {
-	selectedTickets--;
-	updateDisplay();
+  selectedTickets--;
+  updateDisplay();
 }
 
 function buy() {
   const number = (Math.floor(Math.random() * 999) + 460).toString() + '' + (Math.floor(Math.random() * 999) + 100).toString();
-	document.location.href = '/bought.html?ticketCount='+selectedTickets+'&boughtTime='+(Math.floor(Date.now()/1000))+'&number='+number;
+  document.location.href = '/bought.html?ticketCount=' + selectedTickets + '&boughtTime=' + (Math.floor(Date.now() / 1000)) + '&number=' + number;
 }
